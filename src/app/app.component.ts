cc4ca7c9f299f50cd98d9702209df6068cdbe79d
Lyuu17 <lyuu17@yandex.com>
2023-05-23 16:42:33 +0200

    Game listing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+interface IGame {
+  id: number;
+  title: string;
+  description: string;
+  steamId: number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'springmvc-angularjs';
+  games: IGame[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get<IGame[]>(`/api/v1/games/`).subscribe(data => this.games = data);
+  }
 }
