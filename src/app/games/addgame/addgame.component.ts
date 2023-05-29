5e314f58a0a4340f06092025e81a6b9143d22099
Lyuu17 <lyuu17@yandex.com>
2023-05-29 10:00:31 +0200

    Use formgroup to handle the addgame form

--- src/app/games/addgame/addgame.component.ts
+++ src/app/games/addgame/addgame.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -9,9 +9,11 @@ import { Router } from '@angular/router';
 })
 export class AddgameComponent {
 
-  titleInput = new FormControl('');
-  descriptionInput = new FormControl('');
-  steamIdInput = new FormControl(0);
+  form = new FormGroup({
+    title: new FormControl(''),
+    description: new FormControl(''),
+    steamId: new FormControl(0)
+  });
 
   constructor(
     private readonly http: HttpClient,
@@ -20,9 +22,9 @@ export class AddgameComponent {
 
   submit(): void {
     this.http.post(`/api/v1/games/`, {
-      title: 'title',
-      description: 'description',
-      steamId: 0
+      title: this.form.get('title')?.value,
+      description: this.form.get('description')?.value,
+      steamId: this.form.get('steamId')?.value
     }).subscribe(() => this.router.navigateByUrl(`/`));
   }
 }
